c4efb8532da6a616a3bc64336482cb6dc5e35c46
Kyle <kyleryan480@gmail.com>
2018-07-02 20:26:54 -0700

    using flexbox

---
+++ script.js
@@ -0,0 +1,5 @@
+const screen = document.getElementById("screen");
+const entry = document.getElementById("entry");
+const submit = document.getElementById("submit");
+
+submit.addEventListener("click", () => alert("hello"));
