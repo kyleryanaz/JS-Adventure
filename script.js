4bcad092d76dfe14290117e0c62c460624823335
Kyle <kyleryan480@gmail.com>
2018-07-02 22:16:02 -0700

    First question working

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
-const screen = document.getElementById("screen");
+const prompter = document.getElementById("prompter");
 const entry = document.getElementById("entry");
 const submit = document.getElementById("submit");
 
-submit.addEventListener("click", () => alert("hello"));
+let position = 0;
+
+const prompts = [
+  "Goodbye.",
+  "You are just outside the town of Hoban. To \
+  the North is the town gate. To the south is a \
+  dirt road leading away from the town. To the \
+  East and West is a forest of pine trees."
+];
+
+const inventroy = [];
+
+let collectEntry = () => {
+  let uInput = entry.value.toLowerCase();
+  if (position === 0) {
+    switch (uInput) {
+      case "no":
+        prompter.innerHTML = prompts[0];
+        break;
+      case "yes":
+        prompter.innerHTML = prompts[1];
+        break;
+      default:
+        alert("input not recognized");
+    }
+  } else {
+    console.log("position problem");
+  }
+};
+
+submit.addEventListener("click", collectEntry);
