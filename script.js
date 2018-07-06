b2e1ab5c4bd9e4cc0de7cf79e795fbf2aa70fd86
Kyle <kyleryan480@gmail.com>
2018-07-05 18:54:45 -0700

    changed to single quotes, corrected a spelling error

--- script.js
+++ script.js
@@ -1,25 +1,28 @@
-const prompter = document.getElementById("prompter");
-const entry = document.getElementById("entry");
-const submit = document.getElementById("submit");
+const prompter = document.getElementById('prompter');
+const entry = document.getElementById('entry');
+const submit = document.getElementById('submit');
 
 let position = 0;
 
-const greeting = "Welcome. Are you ready to play?";
+const greeting = 'Welcome. Are you ready to play?';
 
 const p00Prompts = [
-  "Goodbye.",
-  "You are just outside the town of Hoban. To \
+  'Goodbye.',
+  'You are just outside the town of Hoban. To \
   the North is the town gate. To the South is a \
   dirt road leading away from the town. To the \
   East and West is a forest of pine trees. What \
-  do you do?"
+  do you do?'
 ];
 
 const p01Prompts = [
-  "You approach the gate.",
-  "You head into the woods East of the road.",
-  "You turn around and start walking away from the town.",
-  "You head inot the woods West of the road."
+  'You approach the gate. An armored guard stands \
+  on each side of the gate. As you approach, one \
+  of them steps forward and asks, "What is your \
+  business in Hoban?"',
+  'You head into the woods East of the road.',
+  'You turn around and start walking away from the town.',
+  'You head into the woods West of the road.'
 ];
 
 var pDex = 0;
@@ -42,51 +45,51 @@ let collectEntry = () => {
   let uInput = entry.value.toLowerCase();
   if (position === 0) {
     switch (uInput) {
-      case "no":
-      prompter.innerHTML = "";
+      case 'no':
+      prompter.innerHTML = '';
         promptWriter(p00Prompts[0]);
         position++;
         break;
-      case "yes":
-      prompter.innerHTML = "";
+      case 'yes':
+      prompter.innerHTML = '';
         promptWriter(p00Prompts[1]);
         position++;
         break;
       default:
-        alert("input not recognized");
+        alert('input not recognized');
     }
   } else if (position === 1) {
     switch (uInput){
-      case "go north":
-      case "walk north":
-      prompter.innerHTML = "";
+      case 'go north':
+      case 'walk north':
+      prompter.innerHTML = '';
         promptWriter(p01Prompts[0]);
         position++;
         break;
-      case "go east":
-      case "walk east":
-      prompter.innerHTML = "";
+      case 'go east':
+      case 'walk east':
+      prompter.innerHTML = '';
         promptWriter(p01Prompts[1]);
         position++;
         break;
-      case "go south":
-      case "walk south":
-      prompter.innerHTML = "";
+      case 'go south':
+      case 'walk south':
+      prompter.innerHTML = '';
         promptWriter(p01Prompts[2]);
         position++;
         break;
-      case "go west":
-      case "walk west":
-      prompter.innerHTML = "";
+      case 'go west':
+      case 'walk west':
+      prompter.innerHTML = '';
         promptWriter(p01Prompts[3]);
         position++;
         break;
       default:
-        alert("position problem");
+        alert('position problem');
     }
   } else {
-    console.log("position problem");
+    console.log('position problem');
   }
 };
 
-submit.addEventListener("click", collectEntry);
+submit.addEventListener('click', collectEntry);
